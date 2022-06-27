70ee50173e2594bc7c8abdabc382f3efe381e0e8
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2022-06-27 08:56:35 -0400

    refactored js and  added function to update chart width on window resize

--- scores.js
+++ scores.js
@@ -1,23 +1,6 @@
 var resetButton = document.querySelector("#clear-storage");
 var highScoreEl = document.querySelector("#score-container")
 
-// iterate localStorage
-function displayScores() {    
-    for (var i = 0; i < localStorage.length; i++) {
-
-        // set iteration key name
-        var key = localStorage.key(i);
-    
-        // use key name to retrieve the corresponding value
-        var value = localStorage.getItem(key);
-
-        var displayScore = document.createElement("li");
-    
-        // Append values to high score section
-        displayScore.textContent = `${key} - ${value}`;
-        highScoreEl.append(displayScore);
-    }
-}
 
 function clearStorage() {
     localStorage.clear();
@@ -26,4 +9,74 @@ function clearStorage() {
 
 resetButton.addEventListener("click", clearStorage)
 
-displayScores();
+let initials = [];
+let scores = [];
+
+for (let index = 0; index < localStorage.length; index++) {
+    let initial = localStorage.key(index);
+    initials.push(initial);
+
+    let score = localStorage.getItem(initial);
+    scores.push(score);
+}
+
+const data = {
+    labels: initials,
+    datasets: [{
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: scores,
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        indexAxis: 'y',
+        scales: {
+            x: {
+                ticks: {
+                    color: "white",
+                    size: 24
+                }
+            },
+            y: {
+                ticks: {
+                    color: "white",
+                    size: 24
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+function getWidth() {
+    if (window.innerWidth > 1600) {
+        let width = .65
+        return width;
+    } else if (window.innerWidth < 768) {
+        let width = .85
+        return width;
+    } else {
+        let width = .75
+        return width;
+    }
+}
+
+function setChartWidth() {
+    const chart = document.querySelector('#chart-container');
+    chart.style.width = (window.innerWidth * getWidth()) + "px"
+}
+
+window.addEventListener("resize", setChartWidth)
